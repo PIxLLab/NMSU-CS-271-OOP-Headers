9f648d318f1977353e7ecb4f06ce357aca9260b7
Phoebe O. Toups Dugas <pdugas@cs.nmsu.edu>
2022-07-12 14:06:04 -0600

    New version of Simple_window to get exiting to work right.
    Updates from: https://groups.google.com/g/ppp-public/c/BtlzdWGuQpQ/m/KuDN4u-SPKgJ?pli=1

--- GUI/Simple_window.h
+++ GUI/Simple_window.h
@@ -7,31 +7,31 @@ using namespace Graph_lib;
 // it provides one window with one "next" button for ultra-simple animation
 
 struct Simple_window : Graph_lib::Window {
-	Simple_window(Point xy, int w, int h, const string& title )
-	: Graph_lib::Window(xy,w,h,title),
+	Simple_window(Point xy, int w, int h, const string& title );
+	/*: Window(xy,w,h,title),
 	  button_pushed(false),
 	  next_button(Point(x_max()-70,0), 70, 20, "Next", cb_next) { attach(next_button); }
-	
-	void wait_for_button()
+	*/
+	bool wait_for_button();
 	// modified event loop
 	// handle all events (as per default), but quit when button_pushed becomes true
 	// this allows graphics without control inversion
-	{
+	/*{
 		while (!button_pushed) Fl::wait();
 		button_pushed = false;
 		Fl::redraw();
-	}
+	}*/
 
 	Button next_button;
 private:
 	bool button_pushed;
 	
-	static void cb_next(Address, Address addr) // callback for next_button
+	static void cb_next(Address, Address addr); // callback for next_button
 	//	{ reference_to<Simple_window>(addr).next(); }
-	{
+	/*{
 		static_cast<Simple_window*>(addr)->next();
-	}
+	}*/
 
-	void next() { button_pushed = true; }
+	void next(); /*{ button_pushed = true; }*/
 
 };
